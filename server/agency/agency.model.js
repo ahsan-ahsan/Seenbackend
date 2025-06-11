1dc16b3335d02a0efd9556452fed22fadaea5245
Astral Developer <157403596+astralDeveper@users.noreply.github.com>
2025-06-11 22:02:21 +0500

    Update agency.model.js

--- server/agency/agency.model.js
+++ server/agency/agency.model.js
@@ -1,7 +1,21 @@
 const mongoose = require("mongoose");
+const { v4: uuidv4 } = require("uuid");
 
+function generateUniqueId(length = 5) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
 const agencySchema = new mongoose.Schema(
   {
+       uniqId: {
+      type: String,
+      default: () => generateUniqueId(),
+      unique: true,
+    },
     image: String,
     agencyName: String,
     agencyTagLine: String,
